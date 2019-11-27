0f9a99ee83df104bb1d07cd7eda329c68f712ee0
zsyh <zsyh9612@sina.com>
2019-11-27 20:36:37 +0800

    init

---
+++ src/main/java/com/zsyhh/hellojenkins/HellojenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.zsyhh.hellojenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellojenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellojenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zsyhh/hellojenkins/HellojenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsyhh.hellojenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellojenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
