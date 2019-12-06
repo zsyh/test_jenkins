4823da4853145566c2728c63c6d7d26a3d8d6e87
zsyh <zsyh9612@gmail.com>
2019-12-06 14:59:39 +0800

    add selenium system test

---
+++ test.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(executable_path='./chromedriver', chrome_options=chrome_options)
+driver.get("http://localhost:8081/get")
+html = driver.find_element_by_tag_name("html")
+print(html.get_attribute('innerHTML'))
