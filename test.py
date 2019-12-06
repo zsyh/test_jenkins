bdab276fb1faa672c314845552b272c6e4306e5c
zsyh <zsyh9612@gmail.com>
2019-12-06 15:01:08 +0800

    add selenium system test

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(executable_path='./chromedriver', chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path='/var/jenkins_home/chromedriver', chrome_options=chrome_options)
 driver.get("http://localhost:8081/get")
 html = driver.find_element_by_tag_name("html")
 print(html.get_attribute('innerHTML'))
